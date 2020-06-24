a2933156ab5aab335ac8f8bc330557d6af2b9f35
Lluis Pitarch <lluispitarchripolles@gmail.com>
2020-06-24 14:52:16 +0200

    building a uploading file to firebase

---
+++ server/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyADzAMcSDLnRlHqSMch4IvUvSNiAJptrKc',
+  authDomain: 'quiz-las-dehesas.firebaseapp.com',
+  databaseURL: 'https://quiz-las-dehesas.firebaseio.com',
+  projectId: 'quiz-las-dehesas',
+  storageBucket: 'quiz-las-dehesas.appspot.com',
+  messagingSenderId: '33252773110',
+  appId: '1:33252773110:web:a27699f2b12acc5a0830d7',
+  measurementId: 'G-CYNQF3Q8MH',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebaseConfig;
