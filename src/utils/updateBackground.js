982d6a93d3abb386e87112049ecf943eb5b904d3
Lluis Pitarch <lluispitarchripolles@gmail.com>
2020-09-07 20:55:17 +0200

    adding some pictures + optimize file size for each one to improve loading resources

--- src/utils/updateBackground.js
+++ src/utils/updateBackground.js
@@ -1,6 +1,12 @@
 const updateBackground = (nextIndex) => {
   const img1 = require('../img/img-quiz-1.jpg');
   const img2 = require('../img/img-quiz-2.jpg');
+  const img3 = require('../img/img-quiz-3.jpg');
+  const img4 = require('../img/img-quiz-4.jpg');
+  const img5 = require('../img/img-quiz-5.jpg');
+  const img6 = require('../img/img-quiz-6.jpg');
+  const img7 = require('../img/img-quiz-7.jpg');
+  const img8 = require('../img/img-quiz-8.jpg');
 
   var imgBg = '';
 
@@ -10,6 +16,24 @@ const updateBackground = (nextIndex) => {
       break;
     case 2:
       imgBg = img2;
+      break;
+    case 3:
+      imgBg = img3;
+      break;
+    case 4:
+      imgBg = img4;
+      break;
+    case 5:
+      imgBg = img5;
+      break;
+    case 6:
+      imgBg = img6;
+      break;
+    case 7:
+      imgBg = img7;
+      break;
+    case 8:
+      imgBg = img8;
       break;
     default:
       imgBg = img1;
